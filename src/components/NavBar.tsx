d3e3f3dd4f8ddea9f159b883ffc9c540ffe53797
Chase Manning <me@chasemanning.co.nz>
2020-12-10 19:17:39 +1300

    :rocket: Create Basic Nav Bar Layout

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,10 +3,20 @@ import { useSelector } from "react-redux";
 import styled from "styled-components";
 import { selectCandies } from "../store/currencySlice";
 import { selectNavBarEnabled } from "../store/navigationSlice";
+import Line from "./Line";
+import Seperator from "./Seperator";
 
 const StyledNavBar = styled.div`
   width: 100%;
   padding: 10px;
+  display: flex;
+  flex-direction: column;
+`;
+
+const Content = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
 `;
 
 const NavBar = () => {
@@ -15,7 +25,17 @@ const NavBar = () => {
 
   if (!navbarEnabled) return null;
 
-  return <StyledNavBar>{candies + " candies"}</StyledNavBar>;
+  return (
+    <StyledNavBar>
+      <Line />
+      <Content>
+        <Seperator />
+        {candies + " candies"}
+        <Seperator />
+      </Content>
+      <Line />
+    </StyledNavBar>
+  );
 };
 
 export default NavBar;
