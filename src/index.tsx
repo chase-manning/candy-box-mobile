7c47800d99be3e60a90d28aff7c1399522786ed7
Chase Manning <me@chasemanning.co.nz>
2020-12-10 17:44:07 +1300

    :lipstick: Change Font to Monospace

--- src/index.tsx
+++ src/index.tsx
@@ -16,7 +16,7 @@ const GlobalStyle = createGlobalStyle`
       box-sizing: border-box;
       margin: 0;
       padding: 0;
-      font-family: "Roboto", sans-serif;
+      font-family: monospace;
   }
 `;
 
