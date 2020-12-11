79fafb40cd4ea48d95cbf138f585030c7c033684
Chase Manning <me@chasemanning.co.nz>
2020-12-11 22:34:13 +1300

    :rocket: Add Village to Map Page

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import styled from "styled-components";
+import village from "../ascii/places/village/village/village";
 import { Page, selectPage } from "../store/navigationSlice";
+import RenderArea from "./RenderArea";
 
 const StyledMap = styled.div`
   width: 100%;
   height: 100%;
+  white-space: pre;
+  overflow: auto;
 `;
 
 const Map = () => {
@@ -13,7 +17,11 @@ const Map = () => {
 
   if (page !== Page.Map) return null;
 
-  return <StyledMap></StyledMap>;
+  return (
+    <StyledMap>
+      <RenderArea area={village} />
+    </StyledMap>
+  );
 };
 
 export default Map;
