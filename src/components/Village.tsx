4e54f5f7983f00d64180a9e592a0f24e77182d75
Chase Manning <me@chasemanning.co.nz>
2020-12-12 18:20:26 +1300

    :rocket: Added Village Smoke

--- src/components/Village.tsx
+++ src/components/Village.tsx
@@ -1,6 +1,72 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { village } from "../ascii/places/village/village/village";
-import RenderArea from "./RenderArea";
+import RenderArea, { Overlay } from "./RenderArea";
 
-const Village = () => <RenderArea area={village} />;
+const Village = () => {
+  let heightModify = 0;
+  let smokeAscii = ["(", ")"];
+  const [smokes, setSmokes] = useState([
+    {
+      area: smokeAscii,
+      x: 8,
+      y: 26,
+    },
+    {
+      area: smokeAscii,
+      x: 64,
+      y: 26,
+    },
+    {
+      area: smokeAscii,
+      x: 80,
+      y: 26,
+    },
+    {
+      area: smokeAscii,
+      x: 59,
+      y: 42,
+    },
+  ]);
+
+  const animate = () => {
+    if (heightModify === 4) {
+      heightModify = 0;
+      smokeAscii = ["(", ")"];
+    } else {
+      heightModify += 1;
+      smokeAscii = smokeAscii.reverse();
+      if (heightModify === 3) smokeAscii = [];
+    }
+    setSmokes([
+      {
+        area: smokeAscii,
+        x: 8,
+        y: 26 - heightModify,
+      },
+      {
+        area: smokeAscii,
+        x: 64,
+        y: 26 - heightModify,
+      },
+      {
+        area: smokeAscii,
+        x: 80,
+        y: 26 - heightModify,
+      },
+      {
+        area: smokeAscii,
+        x: 59,
+        y: 42 - heightModify,
+      },
+    ]);
+
+    setTimeout(() => animate(), 1000);
+  };
+
+  useEffect(() => {
+    animate();
+  }, []);
+
+  return <RenderArea area={village} overlays={smokes} />;
+};
 export default Village;
