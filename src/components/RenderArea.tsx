6f5f52ebb78ec81ef3634073efff9f8595cbcc42
Chase Manning <me@chasemanning.co.nz>
2020-12-12 17:28:25 +1300

    :sparkles: Add Overlay Option to Render Area

--- src/components/RenderArea.tsx
+++ src/components/RenderArea.tsx
@@ -12,14 +12,45 @@ const Line = styled.div`
   width: 100%;
 `;
 
+export type Overlay = {
+  area: string[];
+  x: number;
+  y: number;
+};
+
+const overlayArea = (area: string[], overlay: Overlay): string[] => {
+  let overlayedArea = area;
+  overlay.area.forEach((overlayLine: string, index: number) => {
+    const lineIndex = overlay.y + index;
+    let overlayedAreaLine = overlayedArea[lineIndex];
+    overlayedArea[lineIndex] =
+      overlayedAreaLine.substring(0, overlay.x) +
+      overlayLine +
+      overlayedAreaLine.substring(
+        overlay.x + overlayLine.length,
+        overlayedAreaLine.length
+      );
+  });
+  return overlayedArea;
+};
+
 type Props = {
   area: string[];
+  overlays?: Overlay[];
 };
 
 const RenderArea = (props: Props) => {
+  let overlayedArea = props.area;
+  if (props.overlays) {
+    props.overlays.forEach(
+      (overlay: Overlay) =>
+        (overlayedArea = overlayArea(overlayedArea, overlay))
+    );
+  }
+
   return (
     <StyledRenderArea>
-      {props.area.map((line: string) => (
+      {overlayedArea.map((line: string) => (
         <Line>{line}</Line>
       ))}
     </StyledRenderArea>
