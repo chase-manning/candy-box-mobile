14503423591f37c602a66f6c54c61c7e1fd1e5c4
Chase Manning <me@chasemanning.co.nz>
2020-12-12 23:04:47 +1300

    :sparkles: Create Button Style

---
+++ src/styled/Button.tsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const Button = styled.button`
+  margin-top: 50px;
+  border: solid 1px #464c54;
+  background-color: #dcdad5;
+`;
+
+export default Button;
