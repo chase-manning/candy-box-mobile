3da5e3b8971081b304b608e50bfc7942f314fe85
Chase Manning <me@chasemanning.co.nz>
2020-12-12 23:12:30 +1300

    :rocket: Add Village Back Button

--- src/components/Village.tsx
+++ src/components/Village.tsx
@@ -1,22 +1,33 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import styled from "styled-components";
-import { selectVillagePage, VillagePage } from "../store/navigationSlice";
+import {
+  selectVillagePage,
+  setVillagePage,
+  VillagePage,
+} from "../store/navigationSlice";
+import Button from "../styled/Button";
 import VillageFirstHouse from "./VillageFirstHouse";
 import VillageHome from "./VillageHome";
 
 const StyledVillage = styled.div`
   width: 100%;
   height: 100%;
+  display: flex;
+  flex-direction: column;
 `;
 
 const Village = () => {
+  const dispatch = useDispatch();
   const villagePage = useSelector(selectVillagePage);
 
   return (
     <StyledVillage>
       {villagePage === VillagePage.HOME && <VillageHome />}
       {villagePage === VillagePage.FirstHouse && <VillageFirstHouse />}
+      <Button onClick={() => dispatch(setVillagePage(VillagePage.HOME))}>
+        Back to the village
+      </Button>
     </StyledVillage>
   );
 };
