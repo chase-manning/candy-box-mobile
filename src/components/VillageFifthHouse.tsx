2d34d138b2113439ac98ef7d944ec0ae2b306d14
Chase Manning <me@chasemanning.co.nz>
2020-12-12 23:23:50 +1300

    :sparkles: Create Village Fifth House

---
+++ src/components/VillageFifthHouse.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledVillageFifthHouse = styled.div`
+  width: 100%;
+  height: 100%;
+`;
+
+const VillageFifthHouse = () => {
+  return <StyledVillageFifthHouse>Fifth House</StyledVillageFifthHouse>;
+};
+
+export default VillageFifthHouse;
