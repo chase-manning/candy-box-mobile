f7a55ad6bcd728d31c75f057c87ea797c515961c
Chase Manning <me@chasemanning.co.nz>
2020-12-12 23:12:45 +1300

    :fire: Remove Redundant Conditional Rendering

--- src/components/VillageFirstHouse.tsx
+++ src/components/VillageFirstHouse.tsx
@@ -1,7 +1,5 @@
 import React from "react";
-import { useSelector } from "react-redux";
 import styled from "styled-components";
-import { selectVillagePage, VillagePage } from "../store/navigationSlice";
 
 const StyledVillageFirstHouse = styled.div`
   width: 100%;
@@ -9,10 +7,6 @@ const StyledVillageFirstHouse = styled.div`
 `;
 
 const VillageFirstHouse = () => {
-  const villagePage = useSelector(selectVillagePage);
-
-  if (villagePage !== VillagePage.FirstHouse) return null;
-
   return <StyledVillageFirstHouse>First House</StyledVillageFirstHouse>;
 };
 
