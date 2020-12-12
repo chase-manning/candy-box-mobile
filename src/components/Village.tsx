9d81bc0050d8b99b7afd5be113f0c9852fed517a
Chase Manning <me@chasemanning.co.nz>
2020-12-12 17:27:47 +1300

    :sparkles: Create Village Component

---
+++ src/components/Village.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { village } from "../ascii/places/village/village/village";
+import RenderArea from "./RenderArea";
+
+const Village = () => <RenderArea area={village} />;
+export default Village;
