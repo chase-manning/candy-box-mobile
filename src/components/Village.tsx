6e3c6f4859d5f5d1ce3f2105a0e0bd170a52db6b
Chase Manning <me@chasemanning.co.nz>
2020-12-12 19:35:25 +1300

    :sparkles: Add Buttons for Village Houses

--- src/components/Village.tsx
+++ src/components/Village.tsx
@@ -1,8 +1,34 @@
 import React, { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import styled from "styled-components";
 import { village } from "../ascii/places/village/village/village";
+import { setVillagePage, VillagePage } from "../store/navigationSlice";
 import RenderArea, { Overlay } from "./RenderArea";
 
+const StyledVillage = styled.div`
+  width: 100%;
+  height: 100%;
+  position: relative;
+`;
+
+type ButtonProps = {
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+};
+
+const Button = styled.button`
+  position: absolute;
+  top: ${(props: ButtonProps) => props.y * 15 + "px"};
+  left: ${(props: ButtonProps) => props.x * 7 + "px"};
+  height: ${(props: ButtonProps) => props.height * 15 + "px"};
+  width: ${(props: ButtonProps) => props.width * 7 + "px"};
+`;
+
 const Village = () => {
+  const dispatch = useDispatch();
+
   let heightModify = 0;
   let smokeAscii = ["( ", " )"];
   const [smokes, setSmokes] = useState([
@@ -67,6 +93,52 @@ const Village = () => {
     animate();
   }, []);
 
-  return <RenderArea area={village} overlays={smokes} />;
+  return (
+    <StyledVillage>
+      <RenderArea area={village} overlays={smokes} />
+      <Button
+        onClick={() => dispatch(setVillagePage(VillagePage.FirstHouse))}
+        x={1}
+        y={23}
+        width={13}
+        height={5}
+      />
+      <Button
+        onClick={() => dispatch(setVillagePage(VillagePage.SecondHouse))}
+        x={19}
+        y={19}
+        width={10}
+        height={9}
+      />
+      <Button
+        onClick={() => dispatch(setVillagePage(VillagePage.ThirdHouse))}
+        x={33}
+        y={23}
+        width={8}
+        height={5}
+      />
+      <Button
+        onClick={() => dispatch(setVillagePage(VillagePage.Forge))}
+        x={63}
+        y={23}
+        width={11}
+        height={5}
+      />
+      <Button
+        onClick={() => dispatch(setVillagePage(VillagePage.FourthHouse))}
+        x={79}
+        y={23}
+        width={11}
+        height={5}
+      />
+      <Button
+        onClick={() => dispatch(setVillagePage(VillagePage.FithHouse))}
+        x={94}
+        y={23}
+        width={10}
+        height={5}
+      />
+    </StyledVillage>
+  );
 };
 export default Village;
