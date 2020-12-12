480567e26dae5cc838ab7bb71aa2c77bbef57697
Chase Manning <me@chasemanning.co.nz>
2020-12-12 23:13:17 +1300

    :sparkles: Create Village Second House Component

---
+++ src/components/VillageSecondHouse.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledVillageSecondHouse = styled.div`
+  width: 100%;
+  height: 100%;
+`;
+
+const VillageFirstHouse = () => {
+  return <StyledVillageSecondHouse>Second House</StyledVillageSecondHouse>;
+};
+
+export default VillageFirstHouse;
