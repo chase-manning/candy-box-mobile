4704bcf4f3ce053377efb4728659439943bd579a
Chase Manning <me@chasemanning.co.nz>
2020-12-12 23:22:37 +1300

    :sparkles: Create Village Third House

---
+++ src/components/VillageThirdHouse.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledVillageThirdHouse = styled.div`
+  width: 100%;
+  height: 100%;
+`;
+
+const VillageThirdHouse = () => {
+  return <StyledVillageThirdHouse>Third House</StyledVillageThirdHouse>;
+};
+
+export default VillageThirdHouse;
