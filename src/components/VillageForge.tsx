61fc2956a9723483cb07b79520f71e784f85d9ad
Chase Manning <me@chasemanning.co.nz>
2020-12-12 23:22:53 +1300

    :sparkles: Create Village Forge

---
+++ src/components/VillageForge.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledVillageForge = styled.div`
+  width: 100%;
+  height: 100%;
+`;
+
+const VillageForge = () => {
+  return <StyledVillageForge>Forge</StyledVillageForge>;
+};
+
+export default VillageForge;
