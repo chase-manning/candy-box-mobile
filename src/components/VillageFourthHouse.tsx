e625d440aa10ff347fc7c6d3420996e812d4aad6
Chase Manning <me@chasemanning.co.nz>
2020-12-12 23:23:32 +1300

    :sparkles: Create Village Fourth House

---
+++ src/components/VillageFourthHouse.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledVillageFourthHouse = styled.div`
+  width: 100%;
+  height: 100%;
+`;
+
+const VillageFourthHouse = () => {
+  return <StyledVillageFourthHouse>Fourth House</StyledVillageFourthHouse>;
+};
+
+export default VillageFourthHouse;
