a0a766dabe7fe87cae3f5512e3be8c773a0bf419
Chase Manning <me@chasemanning.co.nz>
2020-12-13 17:31:45 +1300

    :lipstick: Add Second House Background

--- src/components/VillageSecondHouse.tsx
+++ src/components/VillageSecondHouse.tsx
@@ -1,13 +1,20 @@
 import React from "react";
 import styled from "styled-components";
+import { secondHouse } from "../ascii/places/village/village/secondHouse.tsx/secondHouse";
+import RenderArea from "./RenderArea";
 
 const StyledVillageSecondHouse = styled.div`
   width: 100%;
   height: 100%;
+  overflow: auto;
 `;
 
 const VillageSecondHouse = () => {
-  return <StyledVillageSecondHouse>Second House</StyledVillageSecondHouse>;
+  return (
+    <StyledVillageSecondHouse>
+      <RenderArea area={secondHouse} />
+    </StyledVillageSecondHouse>
+  );
 };
 
 export default VillageSecondHouse;
